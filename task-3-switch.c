f214f7aa423cca57a04df804f7ec99e4660d48e3
Yasir Abbasi <pc9304.muhammadyasir@gmail.com>
2026-09-24 23:25:46 +0500

    Task 3 using switch case

---
+++ task-3-switch.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+int main() {
+    float balance = 50000;
+    int choice;
+    float deposit_money;
+    float withdraw_money;
+    printf("1. Check Balance \n");
+    printf("2. Deposit Money \n");
+    printf("3. Withdraw Money \n");
+    printf("4. Exit \n");
+    printf("Enter your choice: \n");
+    scanf("%d", &choice);
+    switch (choice) {
+        case 1:
+            printf("Your current balance is: %f \n", balance);
+            break;
+
+        case 2:
+            printf("Enter the amount you want to deposit: \n");
+            scanf("%f", &deposit_money);
+            balance = balance + deposit_money;
+            printf("Updated Balance = %f \n", balance);
+            break;
+
+        case 3:
+            printf("Enter the amount you want to withdraw: \n");
+            scanf("%f", &withdraw_money);
+            if (withdraw_money<=balance) {
+                balance = balance - withdraw_money;
+                printf("Remaining balance = %f \n", balance);
+            }
+            else {
+                printf("Insufficient balance \n");
+            }
+            break;
+
+        case 4:
+            printf("Thank you for using the ATM \n");
+            break;
+    }
+    return 0;
+}
